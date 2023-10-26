643ae1f7d2e5c9731608d22948a3a4b81b4aece6
Jesus M <jesbenmel@gmail.com>
2023-10-26 08:57:47 -0500

    added function to get a choice from the computer

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
 const getComputerChoice = () => {
+    randomNumber = Math.floor(Math.random * 3);
+    switch (randomNumber) {
+        case 0: return 'rock'
+        break;
+        case 1: return 'paper'
+        break;
+        case 2: return 'scissors'
+        break;
+    };
+};
 
-}
+console.log(getComputerChoice);
