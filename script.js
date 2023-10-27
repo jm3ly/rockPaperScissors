53a23fb5eefde35558173defe18b7a0855589012
Jesus M <jesbenmel@gmail.com>
2023-10-27 09:09:58 -0500

    function returns logic on winner

--- script.js
+++ script.js
@@ -8,4 +8,31 @@ function getComputerChoice () {
     }
 }
 
-console.log(getComputerChoice())
+
+/*function to play a round of rock paper scissors*/
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return 'This game is a tie';
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'rock')
+    ) {
+        return 'User has won!';
+    } else {
+        return 'Computer has won the game';
+    }
+}
+
+function game() {
+
+}
+
+/*my console logs*/     
+const playerSelection = 'pAPEr';
+const computerSelection = getComputerChoice();
+
+console.log(playerSelection);
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection));
